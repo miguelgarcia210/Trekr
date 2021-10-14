a18b62b9854066316738d629d6e64ddadf40fc5f
Miguel Garcia <garciam.code@gmail.com>
2021-10-14 00:15:00 -0500

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Miguel Garcia on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
